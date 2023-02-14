2347408c25356e7db21554a21a971a71ad1fa8f8
ernestodiazkiki <carlos@kikilogistics.co>
2023-02-14 12:05:44 -0500

    :construction: add dependencies and move file to functions

---
+++ src/functions/enterprise/create.ts
@@ -0,0 +1,11 @@
+import {APIGatewayProxyHandler} from "aws-lambda";
+export const handler: APIGatewayProxyHandler = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'ok',
+            id: 2,
+            name: event
+        }),
+    };
+}
